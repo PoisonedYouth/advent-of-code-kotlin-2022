db987410d324915dafdab0b6faa4ec69488335b2
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-12-01 08:28:18 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
+    fun createListOfAmountPerElve(input: List<String>): MutableList<Int> {
+        val elvesAmount = mutableListOf(0)
+        input.forEach {
+            if (it.isNotBlank()) {
+                elvesAmount[elvesAmount.lastIndex] += it.toInt()
+            } else {
+                elvesAmount.add(0)
+            }
+        }
+        return elvesAmount
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elvesAmount = createListOfAmountPerElve(input)
+        return elvesAmount.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elvesAmount = createListOfAmountPerElve(input)
+        return elvesAmount.sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
