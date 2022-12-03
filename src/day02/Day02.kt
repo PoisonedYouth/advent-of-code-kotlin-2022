e0e57805ed133f636cda88566aa1cabce6c7c2d9
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-12-03 07:12:54 +0100

    Day02 (optimization 2)

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -4,116 +4,41 @@ import readInput
 
 fun main() {
 
-
     fun part1(input: List<String>): Int {
-        val inputPairs = input.map { it.split(" ") }.map { Options.valueOfInput(it[0]) to Options.valueOfInput(it[1]) }
-
-        fun calculateResultRock(options: Options): Int {
-            return when (options) {
-                Options.ROCK -> 1 + 3
-                Options.PAPER -> 2 + 6
-                Options.SCISSORS -> 3 + 0
-            }
-        }
-
-        fun calculateResultPaper(options: Options): Int {
-            return when (options) {
-                Options.ROCK -> 1 + 0
-                Options.PAPER -> 2 + 3
-                Options.SCISSORS -> 3 + 6
-            }
-        }
-
-        fun calculateResultScissors(options: Options): Int {
-            return when (options) {
-                Options.ROCK -> 1 + 6
-                Options.PAPER -> 2 + 0
-                Options.SCISSORS -> 3 + 3
-            }
-        }
-
-        return inputPairs.sumOf {
-            when (it.first) {
-                Options.ROCK -> calculateResultRock(it.second)
-                Options.PAPER -> calculateResultPaper(it.second)
-                Options.SCISSORS -> calculateResultScissors(it.second)
-            }
+        return input.sumOf {
+            when (it) {
+                "A X" -> 1 + 3
+                "A Y" -> 2 + 6
+                "A Z" -> 3 + 0
+                "B X" -> 1 + 0
+                "B Y" -> 2 + 3
+                "B Z" -> 3 + 6
+                "C X" -> 1 + 6
+                "C Y" -> 2 + 0
+                "C Z" -> 3 + 3
+                else -> error("Invalid input!")
+            }.toInt()
         }
     }
 
     fun part2(input: List<String>): Int {
-        val inputPairs = input.map { it.split(" ") }.map { Options.valueOfInput(it[0]) to GameResult.valueOfInput(it[1]) }
-
-        fun calculateResultRock(gameResult: GameResult): Int {
-            return when (gameResult) {
-                GameResult.LOOSE -> 3 + 0
-                GameResult.DRAW -> 1 + 3
-                GameResult.WIN -> 2 + 6
-            }
-        }
-
-        fun calculateResultPaper(gameResult: GameResult): Int {
-            return when (gameResult) {
-                GameResult.LOOSE -> 1 + 0
-                GameResult.DRAW -> 2 + 3
-                GameResult.WIN -> 3 + 6
-            }
-        }
-
-        fun calculateResultScissors(gameResult: GameResult): Int {
-            return when (gameResult) {
-                GameResult.LOOSE -> 2 + 0
-                GameResult.DRAW -> 3 + 3
-                GameResult.WIN -> 1 + 6
-            }
-        }
-
-        return inputPairs.sumOf {
-            when (it.first) {
-                Options.ROCK -> calculateResultRock(it.second)
-                Options.PAPER -> calculateResultPaper(it.second)
-                Options.SCISSORS -> calculateResultScissors(it.second)
-            }
+        return input.sumOf {
+            when (it) {
+                "A X" -> 3 + 0
+                "A Y" -> 1 + 3
+                "A Z" -> 2 + 6
+                "B X" -> 1 + 0
+                "B Y" -> 2 + 3
+                "B Z" -> 3 + 6
+                "C X" -> 2 + 0
+                "C Y" -> 3 + 3
+                "C Z" -> 1 + 6
+                else -> error("Invalid input!")
+            }.toInt()
         }
     }
 
     val input = readInput("day02/Day02")
     println(part1(input))
     println(part2(input))
-}
-
-enum class Options {
-    ROCK, PAPER, SCISSORS;
-
-    companion object {
-        fun valueOfInput(input: String): Options {
-            return when (input) {
-                "X",
-                "A" -> ROCK
-
-                "Y",
-                "B" -> PAPER
-
-                "Z",
-                "C" -> SCISSORS
-
-                else -> error("Invalid input")
-            }
-        }
-    }
-}
-
-enum class GameResult {
-    LOOSE, DRAW, WIN;
-
-    companion object {
-        fun valueOfInput(input: String): GameResult {
-            return when (input) {
-                "X" -> LOOSE
-                "Y" -> DRAW
-                "Z" -> WIN
-                else -> error("Invalid input!")
-            }
-        }
-    }
 }
