32fcf95d410a35cdc9b7d28663bca18d3e2eb141
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-12-03 09:58:02 +0100

    Day03 (optimization 1)

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -4,22 +4,22 @@ import readInput
 
 fun main() {
 
-    fun calculatePriority(char: Char): Int {
-        return if (char.isUpperCase()) (char - 'A' + 27) else (char - 'a' + 1)
+    fun Char.calculatePriority(): Int {
+        return if (this.isUpperCase()) (this - 'A' + 27) else (this - 'a' + 1)
     }
 
     fun part1(input: List<String>): Int {
         return input.sumOf { line ->
             val (part1, part2) = line.chunked(line.length / 2).map { it.toCharArray().toSet() }
-            part1.intersect(part2).sumOf { calculatePriority(it) }
+            part1.intersect(part2).first().calculatePriority()
         }
-
     }
+
 
     fun part2(input: List<String>): Int {
         return input.chunked(3).sumOf { group ->
             val (elv1, elv2, elv3) = group.map { it.toCharArray().toSet() }
-            elv1.intersect(elv2).intersect(elv3).sumOf { calculatePriority(it) }
+            elv1.intersect(elv2).intersect(elv3).first().calculatePriority()
         }
     }
 
