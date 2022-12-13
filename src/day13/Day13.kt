f6d6776c451e7017ffb1e95749cf82f6c0343c0c
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-12-13 12:34:27 +0100

    Day13 (Optimization 1)

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -4,17 +4,22 @@ import readInput
 
 sealed interface Element : Comparable<Element>
 
-class ElementList(vararg val elements: Element) : Element {
+class ElementList(val elements: List<Element>) : Element {
+    constructor(element: Element) : this(listOf(element))
+
     override fun compareTo(other: Element): Int = when (other) {
-        is Value -> compareTo(ElementList(other))
-        is ElementList -> elements.zip(other.elements, Element::compareTo).firstOrNull { it != 0 }
-            ?: elements.size.compareTo(other.elements.size)
+        is ValueElement -> compareTo(ElementList(other))
+        is ElementList -> {
+            elements.zip(other.elements).map {
+                it.first.compareTo(it.second)
+            }.firstOrNull { it != 0 } ?: this.elements.size.compareTo(other.elements.size)
+        }
     }
 }
 
-class Value(val int: Int) : Element {
+class ValueElement(val value: Int) : Element {
     override fun compareTo(other: Element): Int = when (other) {
-        is Value -> int.compareTo(other.int)
+        is ValueElement -> value.compareTo(other.value)
         is ElementList -> ElementList(this).compareTo(other)
     }
 }
@@ -27,8 +32,8 @@ private fun mapToElement(line: String): Element {
         line.split(regex).filter(String::isNotBlank).forEach { t ->
             when (t) {
                 "[" -> add(mutableListOf())
-                "]" -> removeLast().also { last().add(ElementList(*it.toTypedArray())) }
-                else -> last().add(Value(t.toInt()))
+                "]" -> removeLast().also { last().add(ElementList(it)) }
+                else -> last().add(ValueElement(t.toInt()))
             }
         }
     }
@@ -45,8 +50,8 @@ fun main() {
 
     fun part2(input: List<String>): Int {
         val parsed = input.filter(String::isNotBlank).map(::mapToElement)
-        val packet1 = ElementList(ElementList(Value(2)))
-        val packet2 = ElementList(ElementList(Value(6)))
+        val packet1 = ElementList(ElementList(ValueElement(2)))
+        val packet2 = ElementList(ElementList(ValueElement(6)))
         val list = (parsed + packet1 + packet2).sorted()
         return (1 + list.indexOf(packet1)) * (1 + list.indexOf(packet2))
     }
